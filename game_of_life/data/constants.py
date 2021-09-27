5bae6550ec949e330afa8d8e03e0cf5c75416669
ethancharles02 <ethan.charles02@gmail.com>
2021-09-27 14:56:39 -0600

    Updated constants

--- game_of_life/data/constants.py
+++ game_of_life/data/constants.py
@@ -6,6 +6,8 @@ SCREEN_TITLE = "Conway's Game of Life"
 
 # I recommend having the ratio between screen and board be 1:1 so you get squares.
 # It functions just fine either way but it will look weird
+# If your screen is small, I recommend making the screen width and screen height smaller,
+#   especially if your display settings are set to show higher than 100% scale
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 BOARD_WIDTH = 35
